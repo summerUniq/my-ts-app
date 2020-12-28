54ac1c470030e77325820b7af2a234b7f2445b62
qin.xia <qin.xia@zhangmen.com>
2020-12-28 14:19:04 +0800

    test

--- src/pages/helloWorld.ts
+++ src/pages/helloWorld.ts
@@ -1,7 +1,7 @@
 const a: string = '1';
-function helloWorld(name: string): void {
+function HelloWorld(name: string): void {
   console.log(`hello ${name}`);
 }
 
-helloWorld('11111');
+HelloWorld('11111');
 console.log(11111);
